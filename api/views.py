4f21ce33e72180e8a6e27cdf6a4a3d03581c5e2f
Jagannath Natarajan <jagnat4000@gmail.com>
2017-12-30 18:30:09 -0800

    Implement DELETE, add some more sanitizing

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.utils import dateparse
+from django.db.models import F
 
 from datetime import datetime, time, timedelta
 import json
@@ -15,7 +16,7 @@ days = {0:[time(hour=8), time(hour=20)],
     3:[time(hour=14), time(hour=20)]}
 
 #TODO: Handle holidays, changes of schedule
-def datetime_valid(start_time):
+def start_time_valid(start_time):
     try:
         datetime_typed = dateparse.parse_datetime(start_time)
     except:
@@ -26,28 +27,41 @@ def datetime_valid(start_time):
 
     return days[week_index][0] <= time_portion and time_portion <= days[week_index][1]
 
+def w_num_valid(w_num):
+    return len(str(w_num)) <= 8
+
+def construct_response(message, error=False, status=200, data=None):
+    response = {'status':'error' if error else 'success', 'message':message}
+    if data is not None:
+        response.update({'data':data})
+    return JsonResponse(response, status=status)
+
+# TODO: Make sure each required JSON parameter exists
+
 @csrf_exempt
 def lfp_api_event(request):
     if (request.method != 'POST' and request.method != 'DELETE'):
-        return JsonResponse({'status':'error', 'message':'Invalid method! Must use POST or DELETE'}, status=405)
+        return construct_response('Invalid method! Must use POST or DELETE', error=True, status=405)
+
+    calendar = LfpAppointment.objects.select_for_update()
+
+    try:
+        jsonRes = json.loads(request.body)
+    except:
+        return construct_response('Failed to parse JSON body!', error=True, status=400)
 
     if (request.method == 'POST'):
-        try:
-            jsonRes = json.loads(request.body)
-        except:
-            return JsonResponse({'status':'error', 'message':'Failed to parse JSON body!'}, status=400)
+        if not start_time_valid(jsonRes['start_time']) and not request.user.is_authenticated():
+            return construct_response('Start time was invalid!', error=True, status=400)
 
-        #TODO: Verify, check for fill status
-        calendar = LfpAppointment.objects.select_for_update()
-
-        if not datetime_valid(jsonRes['start_time']) and not request.user.is_authenticated():
-            return JsonResponse({'status':'error', 'message':'start time was invalid!'}, status=400)
+        if not w_num_valid(jsonRes['w_num']):
+            return construct_response('Invalid W number!', error=True, status=400)
 
         filled_slots = calendar.filter(start_time__exact=jsonRes['start_time']).count()
         if (filled_slots >= 4 and not request.user.is_authenticated()):
-            return JsonResponse({'status':'error', 'message':'Appointment slot is already filled!'})
+            return construct_response('Appointment slot is already filled!', error=True, status=403)
 
-        #TODO: Sanitize
+        #TODO: Sanitize more
         appt = LfpAppointment(
             start_time=jsonRes['start_time'], 
             name=jsonRes['name'],
@@ -59,21 +73,33 @@ def lfp_api_event(request):
             creator=jsonRes['creator'])
         appt.save()
 
-        return JsonResponse({'status':'success','message':'Created event!'})
+        return construct_response('Created event!')
     else: # Method is DELETE
         if (not request.user.is_authenticated()):
-            return JsonResponse({'status':'error', 'message':'You must be logged in to delete an event!'}, status=401)
-        return JsonResponse({'status':'success','message':'Deleted event!'})
+            return construct_response('You must be logged in to delete an event!', error=True, status=401)
+
+        timeslot_events = calendar.filter(start_time__exact=jsonRes['start_time'])
+        target_priority = timeslot_events.filter(priority__exact=jsonRes['priority'])
+
+        if (target_priority.count() == 0):
+            return construct_response('Couldn\'t find event with right priority and time!', error=True, status=404)
+
+        target_priority.delete()
+
+        # Lower priorities of events during same timeslot
+        greater_priorities = timeslot_events.filter(priority__gt=jsonRes['priority'])
+        greater_priorities.update(priority=F('priority') - 1)
+        return construct_response('Deleted event!')
 
 @csrf_exempt
 def lfp_api_calendar(request):
     if (request.method != 'GET'):
-        return JsonResponse({'status':'error', 'message':'Invalid method! Must use GET'}, status=405)
+        return construct_response('Invalid method! Must use GET', error=True, status=405)
 
     anonymize = True
-    if (request.GET.get('full_data', '') == 'true'):
+    if (request.GET.get('show_full', '') == 'true'):
         if (not request.user.is_authenticated()):
-            return JsonResponse({'status':'error', 'message':'You must be logged in to use full_data!'}, status=403)
+            return construct_response('You must be logged in to use show_full!', error=True, status=403)
         anonymize = False
 
     jsonData = []
@@ -88,4 +114,4 @@ def lfp_api_calendar(request):
                 'w_num':appt.w_num,
                 'creator':appt.creator})
         jsonData.append(data)
-    return JsonResponse({'status':'success', 'data':jsonData, 'message':'Requested calendar'})
+    return construct_response('Calendar request successful!', data=jsonData)
